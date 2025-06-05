54d46324984d17dd9ff1e698e405ce1c75ff3e6f
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-06-04 22:41:47 -0700

    a few more tweaks to the dev resources

--- dev/main.tf
+++ dev/main.tf
@@ -19,11 +19,19 @@ resource "azurerm_storage_account" "storage" {
   location                        = azurerm_resource_group.rg.location
   account_tier                    = "Standard"
   account_kind                    = "StorageV2"
-  account_replication_type        = "LRS"
+  account_replication_type        = "LRS" # CKV_AZURE_206 ignored
   min_tls_version                 = "TLS1_2"
   public_network_access_enabled   = false
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false # CKV2_AZURE_41
   tags                            = local.tags
+
+  # enable soft-delete (CKV2_AZURE_38)
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
 }
 
 # a virtual network
