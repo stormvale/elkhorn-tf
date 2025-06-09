7b5aa2552696e922200e4e0201f7085d832ea159
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-06-08 23:06:46 -0700

    trying to resolve some chekov warnings

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -6,12 +6,13 @@ resource "azurerm_storage_account" "storage" {
   access_tier                     = "Hot"
   public_network_access_enabled   = false
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = true
   account_kind                    = "StorageV2"
   account_tier                    = var.account_tier
   account_replication_type        = var.account_replication_type
 
   sas_policy {
-    expiration_period = "01.12:00:00"
+    expiration_period = "01.12:00:00" # dd.HH.mm.ss
   }
 
   tags = merge(
