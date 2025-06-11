f6d441ec5061ef44060a268191b68164e84dc431
Kevin Reid <644802+codeswithfists@users.noreply.github.com>
2025-06-10 21:37:07 -0700

    added tips on swapping between remote and local backends

--- development/terraform.tf
+++ development/terraform.tf
@@ -19,6 +19,11 @@ terraform {
     use_oidc         = true
   }
 
+  # to switch to local backend from dev env:
+  #  1. terraform init -backend-config="storage_account_name=<storage_account_name>" -backend-config="container_name=tfstate" -backend-config="resource_group_name=<resource_group_name>"
+  #  2. <switch backends in terraform.tf>
+  #  3. terraform init -migrate-state
+  #  4. terraform plan should return 'No changes. Your infrastructure matches the configuration.'
   # backend "local" {
   #   path = "development.tfstate"
   # }
